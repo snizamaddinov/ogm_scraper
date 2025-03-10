91f7f273c1b0b7b6eaeda8e0bc4544ac42b44a24
Shanazar <snizamaddinow@gmail.com>
2025-03-10 09:48:48 +0300

    refactor: books with no filtering.

---
+++ scrapers/ektilesimli_kitaplar_menu_no_filtering.py
@@ -0,0 +1,17 @@
+from scrapers.books_with_no_filtering_base_scraper import  BooksWithNoFilteringBaseScraper
+
+
+class SporLisesiKitapScraper(BooksWithNoFilteringBaseScraper):
+    PATH = '/etkilesimli-kitap/spor-lisesi'
+    FILE_NAME_PREFIX = 'Spor_Lisesi_Kitaplari'
+
+    def __init__(self):
+        super().__init__(check_redirect_links=False)
+
+
+class LiseyeHosGeldinScraper(BooksWithNoFilteringBaseScraper):
+    PATH = '/liseye-hosgeldin'
+    FILE_NAME_PREFIX = 'Liseye_Hosgeldin_Kitaplari'
+
+    def __init__(self):
+        super().__init__(check_redirect_links=True)
--- scrapers/spor_lisesi_kitap_scraper.py
+++
@@ -1,9 +0,0 @@
-from scrapers.books_with_no_filtering_base_scraper import  BooksWithNoFilteringBaseScraper
-
-
-class SporLisesiKitapScraper(BooksWithNoFilteringBaseScraper):
-    PATH = '/etkilesimli-kitap/spor-lisesi'
-    FILE_NAME_PREFIX = 'Spor_Lisesi_Kitaplari'
-
-    def __init__(self):
-        super().__init__(check_redirect_links=False)
