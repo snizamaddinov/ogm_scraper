a587051d1581c0e59d72c144f09550e3734175ba
Shanazar <snizamaddinow@gmail.com>
2025-03-10 09:40:13 +0300

    books with progressive filtering.

--- scrapers/defterim_scraper.py
+++
@@ -1,8 +0,0 @@
-from scrapers.books_with_progressive_filtering_base_scraper import BooksWithProgressiveFilteringBaseScraper
-
-class DefterimScraper(BooksWithProgressiveFilteringBaseScraper):
-    PATH = '/defterim'
-    FILE_NAME_PREFIX = 'Defterim_Kitaplari'
-
-    def __init__(self):
-        super().__init__(is_initial_ders=False, check_redirect_links=True)
